d3b14d153a1f59a29791256f075b0cfacc8b6622
Brad Hannah <brad.hannah@gmail.com>
2019-08-24 10:27:45 -0400

    Added very basic tiledata JSON file import. Dependancies are  a mess now...

--- U5ConversationSimulator/Program.cs
+++ U5ConversationSimulator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Ultima5Redux;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace U5ConversationSimulator
 {
@@ -8,7 +10,7 @@ namespace U5ConversationSimulator
         static void Main(string[] args)
         {
             World world = new World("C:\\games\\ultima_5_late\\bucden4");
-
+            List<TileReference> tileReference = TileReference.Load();
 
         }
     }
---
+++ Ultima5Redux/TileReference.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Ultima5Redux
+{
+    [DataContract]  
+    public class TileReference
+    {
+        [DataMember]
+        public int Index;
+        [DataMember]
+        public string Name;
+        [DataMember]
+        public string Description;
+        [DataMember]
+        public bool IsWalking_Passable;
+        [DataMember]
+        public bool IsBoat_Passable;
+        [DataMember]
+        public bool IsSkiff_Passable;
+        [DataMember]
+        public bool IsCarpet_Passable;
+        [DataMember]
+        public bool IsPartOfAnimation;
+        [DataMember]
+        public int AnimationIndex;
+        [DataMember]
+        public bool IsUpright;
+        [DataMember]
+        public int FlatTileSubstitionIndex;
+        [DataMember]
+        public string FlatTileSubstitionName;
+        [DataMember]
+        public bool IsEnemy;
+        [DataMember]
+        public bool IsNPC;
+        [DataMember]
+        public bool IsBuilding;
+
+
+        static public List<TileReference> Load()
+        {
+            //DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(TileReference));
+
+            //MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(Properties.Resources.TileData));
+            //            FileStream fs = new FileStream("Data\\TileData.json",FileMode.Open);
+            List<TileReference> result = JsonConvert.DeserializeObject<List<TileReference>>(Properties.Resources.TileData);
+
+            //TileReference tr = js.ReadObject(ms) as TileReference;
+            //return tr;
+            return result;
+        }
+    }
+}
