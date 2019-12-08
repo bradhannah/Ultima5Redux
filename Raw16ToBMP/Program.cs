fba27c1c6501efc5f396c072a02f0414d9e4bc2a
Brad Hannah <brad.hannah@gmail.com>
2019-12-07 21:43:01 -0500

    Fire export, ultima logo export

--- Raw16ToBMP/Program.cs
+++ Raw16ToBMP/Program.cs
@@ -12,32 +12,7 @@ namespace Raw16ToBMP
 {
     static class BitmapWriter
     {
-        static public Dictionary<byte, Color> GetEGAPalette2()
-        { 
-            Dictionary<byte, Color> palette = new Dictionary<byte, Color>();
-            palette.Add(0, Color.Black);
-            palette.Add(0xFF, Color.FromArgb(0xFF, 0xFF, 0xFF)); // bright white
-
-            palette.Add(64, Color.Red);
-            palette.Add(12, Color.FromArgb(0xFF, 0x55, 0x55)); // bright red
-
-
-            palette.Add(1, Color.Blue);
-            palette.Add(2, Color.Green);
-            palette.Add(3, Color.Cyan);
-            palette.Add(5, Color.Magenta);
-            palette.Add(6, Color.Brown);
-            palette.Add(7, Color.LightGray);
-            palette.Add(8, Color.DarkGray);
-            palette.Add(9, Color.FromArgb(0x55, 0x55, 0xFF)); // bright blue
-            palette.Add(10, Color.FromArgb(0x55, 0xFF, 0x55)); // bright green
-            palette.Add(11, Color.FromArgb(0x55, 0xFF, 0x55)); // bright cyan
-            palette.Add(13, Color.FromArgb(0xFF, 0xFF, 0x55)); // bright magenta
-            palette.Add(14, Color.FromArgb(0xFF, 0x55, 0xFF)); // bright yellow
-
-            return palette;
-        }
-
+  
     static public Dictionary<byte, Color> GetEGAPalette()
         {
             Dictionary<byte, Color> palette=new Dictionary<byte, Color>();
@@ -45,25 +20,17 @@ namespace Raw16ToBMP
             palette.Add(1, Color.Blue);
             palette.Add(2, Color.Green);
             palette.Add(3, Color.Cyan);
-            palette.Add(4, Color.Red);
+            palette.Add(4, Color.FromArgb(0xb51000));//Color.Red);
             palette.Add(5, Color.Magenta);
-            palette.Add(6, Color.Brown);
+            palette.Add(6, Color.FromArgb(0xAF5800));//Color.Brown);
             palette.Add(7, Color.LightGray);
-            palette.Add(8, Color.DarkGray);
-            //palette.Add(56, Color.DarkGray);
-            //palette.Add(57, Color.FromArgb(0x55, 0x55, 0xFF)); // bright blue
-            //palette.Add(58, Color.FromArgb(0x55, 0xFF, 0x55)); // bright green
-            //palette.Add(59, Color.FromArgb(0x55, 0xFF, 0x55)); // bright cyan
-            //palette.Add(60, Color.FromArgb(0xFF, 0x55, 0x55)); // bright red
-            //palette.Add(61, Color.FromArgb(0xFF, 0xFF, 0x55)); // bright magenta
-            //palette.Add(62, Color.FromArgb(0xFF, 0x55, 0xFF)); // bright yellow
-            //palette.Add(63, Color.FromArgb(0xFF, 0xFF, 0xFF)); // bright white
+            palette.Add(8, Color.FromArgb(0x55, 0x55, 0x55));//Color.DarkGray);
             palette.Add(9, Color.FromArgb(0x55, 0x55, 0xFF)); // bright blue
             palette.Add(10, Color.FromArgb(0x55, 0xFF, 0x55)); // bright green
-            palette.Add(11, Color.FromArgb(0x55, 0xFF, 0x55)); // bright cyan
+            palette.Add(11, Color.FromArgb(0x55, 0xFF, 0xFF)); // bright cyan
             palette.Add(12, Color.FromArgb(0xFF, 0x55, 0x55)); // bright red
             palette.Add(13, Color.FromArgb(0xFF, 0xFF, 0x55)); // bright magenta
-            palette.Add(14, Color.FromArgb(0xFF, 0x55, 0xFF)); // bright yellow
+            palette.Add(14, Color.FromArgb(0xFF, 0xFF, 0x55)); // bright yellow
             palette.Add(15, Color.FromArgb(0xFF, 0xFF, 0xFF)); // bright white
 
             return palette;
@@ -116,56 +83,57 @@ namespace Raw16ToBMP
             return 1;
         }
 
-        public static int WriteBitmapFile(string filename, int width, int height, byte[] imageData)
-        {
-            byte[] newData = new byte[imageData.Length];
+    //    public static int WriteBitmapFile(string filename, int width, int height, byte[] imageData)
+    //    {
+    //        byte[] newData = new byte[imageData.Length];
 
-            for (int x = 0; x < imageData.Length; x += 4)
-            {
-                byte[] pixel = new byte[4];
-                Array.Copy(imageData, x, pixel, 0, 4);
+    //        for (int x = 0; x < imageData.Length; x += 4)
+    //        {
+    //            byte[] pixel = new byte[4];
+    //            Array.Copy(imageData, x, pixel, 0, 4);
 
-                byte r = pixel[0];
-                byte g = pixel[1];
-                byte b = pixel[2];
-                byte a = pixel[3];
+    //            byte r = pixel[0];
+    //            byte g = pixel[1];
+    //            byte b = pixel[2];
+    //            byte a = pixel[3];
 
-                byte[] newPixel = new byte[] { b, g, r, a };
+    //            byte[] newPixel = new byte[] { b, g, r, a };
 
-                Array.Copy(newPixel, 0, newData, x, 4);
-            }
+    //            Array.Copy(newPixel, 0, newData, x, 4);
+    //        }
 
-            imageData = newData;
+    //        imageData = newData;
 
-            using (var stream = new MemoryStream(imageData))
-            using (var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb))
-            {
-                BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0,
-                                                                bmp.Width,
-                                                                bmp.Height),
-                                                  ImageLockMode.WriteOnly,
-                                                  bmp.PixelFormat);
+    //        using (var stream = new MemoryStream(imageData))
+    //        using (var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb))
+    //        {
+    //            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0,
+    //                                                            bmp.Width,
+    //                                                            bmp.Height),
+    //                                              ImageLockMode.WriteOnly,
+    //                                              bmp.PixelFormat);
 
-                IntPtr pNative = bmpData.Scan0;
-                Marshal.Copy(imageData, 0, pNative, imageData.Length);
+    //            IntPtr pNative = bmpData.Scan0;
+    //            Marshal.Copy(imageData, 0, pNative, imageData.Length);
 
-                bmp.UnlockBits(bmpData);
+    //            bmp.UnlockBits(bmpData);
 
-                bmp.Save(filename);
-            }
+    //            bmp.Save(filename);
+    //        }
 
-            return 1;
-        }
+    //        return 1;
+    //    }
     }
 
 
     class Program
     {
  
-        static void Main(string[] args)
+        static void CreateScreen()
         {
             const int INDEX_SIZE = 0x32;
             byte[] fileArray = File.ReadAllBytes("C:\\games\\ultima_5\\temp\\dec_res\\create.16.uncomp");
+            //byte[] fileArray = File.ReadAllBytes("C:\\games\\ultima_5\\temp\\dec_res\\create.16.uncomp");
 
             byte[] positionArray = new byte[INDEX_SIZE];
             for (int i = 0; i < positionArray.Length; i++)
@@ -173,19 +141,87 @@ namespace Raw16ToBMP
                 positionArray[i] = fileArray[i];
             }
 
-            int nForcedWidth = 168 * 2;
-            int nForcedHeigh = 31;
+            int nForcedWidth = 168;// * 2;
+            int nForcedHeigh = 200;
             byte[] flameGraphic = new byte[nForcedWidth * nForcedHeigh];//positionArray[2] * positionArray[3]];
 
             int byteIndex = 0;
-            for (int bmpIndex = 0; byteIndex < nForcedWidth * nForcedHeigh; bmpIndex+=2, byteIndex +=1)
+            for (int bmpIndex = 0; byteIndex < nForcedWidth * nForcedHeigh; bmpIndex += 2, byteIndex += 1)
             {
                 flameGraphic[byteIndex] = fileArray[byteIndex + INDEX_SIZE];
             }
 
 
             BitmapWriter.Write16BitmapFile("C:\\games\\ultima_5\\temp\\dec_res\\create.16.bmp", nForcedWidth, nForcedHeigh, flameGraphic);
-                //positionArray[2], positionArray[3], fileArray);
+            //positionArray[2], positionArray[3], fileArray);
+        }
+
+        public static void StartScreen()
+        {
+            Export("C:\\games\\ultima_5\\temp\\dec_res\\startsc.16.uncomp", "C:\\games\\ultima_5\\temp\\dec_res\\startsc.16.bmp",
+                168, 30, 0);
+        }
+
+        public static void UltimaScreen()
+        {
+            Export("C:\\games\\ultima_5\\temp\\dec_res\\ultima.16.uncomp", "C:\\games\\ultima_5\\temp\\dec_res\\ultima.16.bmp",
+                320, 61, 0x1A);
+        }
+
+        public static void FireScreen()
+        {
+            const int nDefaultOffset = 0x04;
+            const int nPixelWidth = 288;
+            const int nBytesWidth = nPixelWidth / 2;
+            const int nPixelHeight = 49;
+            const int nStartIndex = (320 / 2) * 61 + 0x1A;
+            const int nOffsetPerFire = nBytesWidth * nPixelHeight + nDefaultOffset;
+
+            Export("C:\\games\\ultima_5\\temp\\dec_res\\ultima.16.uncomp", "C:\\games\\ultima_5\\temp\\dec_res\\ultima_fire.16.bmp",
+                nPixelWidth, nPixelHeight, nStartIndex + nDefaultOffset);
+
+            Export("C:\\games\\ultima_5\\temp\\dec_res\\ultima.16.uncomp", "C:\\games\\ultima_5\\temp\\dec_res\\ultima_fire2.16.bmp",
+                nPixelWidth, nPixelHeight, nStartIndex + nOffsetPerFire + nDefaultOffset);
+
+            Export("C:\\games\\ultima_5\\temp\\dec_res\\ultima.16.uncomp", "C:\\games\\ultima_5\\temp\\dec_res\\ultima_fire3.16.bmp",
+                nPixelWidth, nPixelHeight, nStartIndex + (nOffsetPerFire*2) + nDefaultOffset);
+
+            Export("C:\\games\\ultima_5\\temp\\dec_res\\ultima.16.uncomp", "C:\\games\\ultima_5\\temp\\dec_res\\ultima_fire4.16.bmp",
+                nPixelWidth, nPixelHeight, nStartIndex + (nOffsetPerFire * 3) + nDefaultOffset);
+        }
+
+
+        static public void Export(string uncompFilename, string bmpFilename, int nGraphicWidth, int nGraphicHeight, int nOffset)
+        {
+            //const int INDEX_SIZE = 0x32;
+            byte[] fileArray = File.ReadAllBytes(uncompFilename);
+
+            byte[] positionArray = new byte[nOffset];
+            for (int i = 0; i < positionArray.Length; i++)
+            {
+                positionArray[i] = fileArray[i];
+            }
+
+            int nForcedWidth = nGraphicWidth;
+            int nForcedHeigh = nGraphicHeight;
+            byte[] flameGraphic = new byte[nForcedWidth * nForcedHeigh];
+
+            int byteIndex = 0;
+            for (int bmpIndex = 0; bmpIndex < nForcedWidth * nForcedHeigh; bmpIndex += 2, byteIndex += 1)
+            {
+                flameGraphic[byteIndex] = fileArray[byteIndex + nOffset];
+            }
+
+
+            BitmapWriter.Write16BitmapFile(bmpFilename, nForcedWidth, nForcedHeigh, flameGraphic);
+        }
+
+
+        static void Main(string[] args)
+        {
+            StartScreen();
+            UltimaScreen();
+            FireScreen();
         }
     }
 }
