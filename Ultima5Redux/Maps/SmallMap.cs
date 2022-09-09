4fe5d9eb24dc1e083c15c69910a5cd0787c47756
Brad Hannah <47454550+bradhannah@users.noreply.github.com>
2022-09-09 07:45:53 -0400

    optimized common call

--- Ultima5Redux/Maps/SmallMap.cs
+++ Ultima5Redux/Maps/SmallMap.cs
@@ -13,21 +13,32 @@ namespace Ultima5Redux.Maps
         public const int X_TILES = 32;
         public const int Y_TILES = 32;
 
+        [IgnoreDataMember]
+        public override SmallMapReferences.SingleMapReference CurrentSingleMapReference
+        {
+            get
+            {
+                if (_currentSingleMapReference == null)
+                    _currentSingleMapReference =
+                        GameReferences.SmallMapRef.GetSingleMapByLocation(MapLocation, MapFloor);
+                return _currentSingleMapReference;
+            }
+        }
+
+        [IgnoreDataMember] public override bool IsRepeatingMap => false;
+
         [IgnoreDataMember] public override int NumOfXTiles => CurrentSingleMapReference.XTiles;
         [IgnoreDataMember] public override int NumOfYTiles => CurrentSingleMapReference.YTiles;
 
         [IgnoreDataMember] public override bool ShowOuterSmallMapTiles => true;
 
         [IgnoreDataMember] public override byte[][] TheMap { get; protected set; }
-
-        [IgnoreDataMember] public override bool IsRepeatingMap => false;
-
-        public override SmallMapReferences.SingleMapReference CurrentSingleMapReference =>
-            GameReferences.SmallMapRef.GetSingleMapByLocation(MapLocation, MapFloor);
+        private SmallMapReferences.SingleMapReference _currentSingleMapReference;
 
         [JsonConstructor] private SmallMap()
         {
         }
+
 
         /// <summary>
         ///     Creates a small map object using a pre-defined map reference
@@ -49,47 +60,9 @@ namespace Ultima5Redux.Maps
             InitializeAStarMap(WalkableType.StandardWalking);
         }
 
-        /// <summary>
-        ///     Calculates an appropriate A* weight based on the current tile as well as the surrounding tiles
-        /// </summary>
-        /// <param name="xy"></param>
-        /// <returns></returns>
-        protected override float GetAStarWeight(in Point2D xy)
-        {
-            bool isPreferredIndex(int nSprite) =>
-                nSprite == GameReferences.SpriteTileReferences.GetTileReferenceByName("BrickFloor").Index ||
-                GameReferences.SpriteTileReferences.IsPath(nSprite);
-
-            const int fDefaultDeduction = 2;
-
-            float fCost = 10;
-
-            // we reduce the weight for the A* for each adjacent brick floor or path tile
-            if (xy.X - 1 >= 0) fCost -= isPreferredIndex(TheMap[xy.X - 1][xy.Y]) ? fDefaultDeduction : 0;
-            if (xy.X + 1 < CurrentSingleMapReference.XTiles)
-                fCost -= isPreferredIndex(TheMap[xy.X + 1][xy.Y]) ? fDefaultDeduction : 0;
-            if (xy.Y - 1 >= 0) fCost -= isPreferredIndex(TheMap[xy.X][xy.Y - 1]) ? fDefaultDeduction : 0;
-            if (xy.Y + 1 < CurrentSingleMapReference.YTiles)
-                fCost -= isPreferredIndex(TheMap[xy.X][xy.Y + 1]) ? fDefaultDeduction : 0;
-
-            return fCost;
-        }
-
         internal override void ProcessTileEffectsForMapUnit(TurnResults turnResults, MapUnit mapUnit)
         {
             // TBD
-        }
-
-        protected internal override WalkableType GetWalkableTypeByMapUnit(MapUnit mapUnit)
-        {
-            return mapUnit switch
-            {
-                Enemy enemy => enemy.EnemyReference.IsWaterEnemy
-                    ? WalkableType.CombatWater
-                    : WalkableType.StandardWalking,
-                CombatPlayer _ => WalkableType.StandardWalking,
-                _ => WalkableType.StandardWalking
-            };
         }
 
         /// <summary>
@@ -123,6 +96,44 @@ namespace Ultima5Redux.Maps
 
             // fill outside of the bounds with a default tile
             return (xInBounds && yInBounds);
+        }
+
+        protected internal override WalkableType GetWalkableTypeByMapUnit(MapUnit mapUnit)
+        {
+            return mapUnit switch
+            {
+                Enemy enemy => enemy.EnemyReference.IsWaterEnemy
+                    ? WalkableType.CombatWater
+                    : WalkableType.StandardWalking,
+                CombatPlayer _ => WalkableType.StandardWalking,
+                _ => WalkableType.StandardWalking
+            };
+        }
+
+        /// <summary>
+        ///     Calculates an appropriate A* weight based on the current tile as well as the surrounding tiles
+        /// </summary>
+        /// <param name="xy"></param>
+        /// <returns></returns>
+        protected override float GetAStarWeight(in Point2D xy)
+        {
+            bool isPreferredIndex(int nSprite) =>
+                nSprite == GameReferences.SpriteTileReferences.GetTileReferenceByName("BrickFloor").Index ||
+                GameReferences.SpriteTileReferences.IsPath(nSprite);
+
+            const int fDefaultDeduction = 2;
+
+            float fCost = 10;
+
+            // we reduce the weight for the A* for each adjacent brick floor or path tile
+            if (xy.X - 1 >= 0) fCost -= isPreferredIndex(TheMap[xy.X - 1][xy.Y]) ? fDefaultDeduction : 0;
+            if (xy.X + 1 < CurrentSingleMapReference.XTiles)
+                fCost -= isPreferredIndex(TheMap[xy.X + 1][xy.Y]) ? fDefaultDeduction : 0;
+            if (xy.Y - 1 >= 0) fCost -= isPreferredIndex(TheMap[xy.X][xy.Y - 1]) ? fDefaultDeduction : 0;
+            if (xy.Y + 1 < CurrentSingleMapReference.YTiles)
+                fCost -= isPreferredIndex(TheMap[xy.X][xy.Y + 1]) ? fDefaultDeduction : 0;
+
+            return fCost;
         }
     }
 }
